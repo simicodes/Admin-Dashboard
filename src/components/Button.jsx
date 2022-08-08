570c816489d1ce49aae5695d269efbd73c4edf1a
simicodes <simeonoffiong@gmail.com>
2022-08-08 17:04:01 +0100

    Ecommerce component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-const Button = () => {
+const Button = ({ color, bgColor, text, size, borderRadius }) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <button
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >{text}</button>
+  );
+};
 
-export default Button
+export default Button;
