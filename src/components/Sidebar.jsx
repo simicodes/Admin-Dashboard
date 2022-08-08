20a6b741bc21c16065c2ad9cc37e825da008ed89
simicodes <simeonoffiong@gmail.com>
2022-08-08 14:51:53 +0100

    Done with both Navbar and Sidebar components, both are responsive and functional - open and close func

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,10 +6,18 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
+import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu, screenSize, setScreenSize } =
+    useStateContext();
 
+  //FUNC
+  const handleCloseSideBar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
+    }
+  };
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2 ";
 
@@ -22,7 +30,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center ">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span></span>
@@ -31,7 +39,9 @@ const Sidebar = () => {
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
                 type="button"
-                onClick={() => {}}
+                onClick={() =>
+                  setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+                }
                 className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
               >
                 <MdOutlineCancel />
@@ -47,7 +57,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
@@ -56,6 +66,8 @@ const Sidebar = () => {
                     <span className="capitalize">{link.name}</span>
                   </NavLink>
                 ))}
+                {/*  */}
+                <div></div>
               </div>
             ))}
           </div>
